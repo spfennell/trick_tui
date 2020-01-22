cccd0886193371f538cd86ff8ce0230314f0b912
Scott Fennell <spfennell@gmail.com>
2020-01-22 16:34:28 -0600

    Update sniffer.cpp

--- sniffer.cpp
+++ sniffer.cpp
@@ -1,8 +1,5 @@
-    /* Receiver/client multicast Datagram example. */
-    //#include <sys/types.h>
-    //#include <sys/socket.h>
+
     #include <arpa/inet.h>
-    //include <netinet/in.h>
     #include <stdio.h>
     #include <iostream>
     #include <string.h>
